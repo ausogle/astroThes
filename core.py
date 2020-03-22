79ca0a74421013da7a914f72cb393c83f9a09683
aogle <aogle@DESKTOP-5AI1RV5.attlocal.net>
2020-03-22 16:41:43 -0400

    Wrote derivative function but was dumb dumb and didn't write test. I will still allow this to be documented

--- core.py
+++ core.py
@@ -4,8 +4,29 @@ from Ffun import f
 from propagator import propagate
 
 
-def __derivative():
-    return np.zeros((6, 2))
+def __direction_isolator(delta, i):
+    m = np.zeros(6)
+    m[i][i] = 1
+    return np.matmul(m, delta)
+
+
+def __derivative(x, dt, params):
+    n = 2
+    m = 6
+    dr = .1
+    dv = .001
+    delta = np. array([dr, dr, dr, dv, dv, dv])
+
+    a = np.zeros(n, m)
+    for j in range(0, m-1):
+        temp1 = propagate(x + __direction_isolator(delta, j), dt, params)
+        temp2 = propagate(x - __direction_isolator(delta, j), dt, params)
+        temp3 = (f(temp1) - f(temp2))/(2*delta[j])
+
+        for i in range(0, n-1):
+            a[i][j] = temp3[i]
+
+    return a
 
 
 def core(x, xoffset, dt, params):
