3bcb0223a992e51e43f53b6955e2e9e5f338a333
aogle <aogle@localhost.localdomain>
2020-04-10 14:18:19 -0400

    Just messing around

---
+++ Ffun.py
@@ -0,0 +1,9 @@
+import math
+import numpy as np
+from numpy import linalg as la
+
+
+def f(rr, obs_params):
+    alpha = math.atan2(rr[1], rr[0]) * 180/math.pi
+    dec = 90 - math.acos(rr[2]/la.norm(rr))*180/math.pi
+    return np.array([alpha, dec])
