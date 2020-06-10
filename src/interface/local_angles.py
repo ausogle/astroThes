982a252e9189ee2c94cba7a24b21aebd27f28477
aogle <aogle@localhost.localdomain>
2020-06-10 18:11:16 -0400

    Added function to get local angles from a state over time with labels for each epoch

--- src/interface/local_angles.py
+++ src/interface/local_angles.py
@@ -1,5 +1,11 @@
 import numpy as np
+from src.state_propagator import state_propagate
+from src.dto import PropParams
+from src.enums import Frames
+from src.frames import lla_to_ecef, ecef_to_lla, ecef_to_eci
 from typing import List
+from astropy.time import Time
+from astropy.coordinates import Angle
 
 
 def local_angles(rr: np.ndarray, lla: List) -> np.ndarray:
@@ -57,3 +63,37 @@ def rotation_matrix(lat, lon):
     :param lon: Longitude of the observer
     """
     return rz(lon) @ ry(lat)
+
+
+def get_local_angles_for_state_propagation(x: np.ndarray, prop_params: PropParams, epoch_f: Time, n: int,
+                                           obs_location, obs_frame: Frames) -> List[List[Angle, Angle, Time]]:
+    """
+    This function returns a list of [theta, phi, Time] from initial epoch in prop_params to final epoch, with an
+    n points between those two epochs.
+
+    :param x: State vector at initial epoch
+    :param prop_params: Parameters relevant to propagation. Includes initial epoch
+    :param epoch_f: Epoch of final desired time
+    :param n: Number of desired points between initial and final epoch
+    :param obs_location: Observer location. Accepts list for LLA or np.array for ECEF
+    :param obs_frame: Frame observer location is in. Accepts Frames.LLA or Frames.ECEF
+    """
+    if obs_frame == Frames.LLA:
+        obs_pos_lla = obs_location
+        obs_pos_ecef = lla_to_ecef(obs_location)
+    else:
+        assert obs_frame == Frames.ECEF
+        obs_pos_lla = ecef_to_lla(obs_location)
+        obs_pos_ecef = obs_location
+
+    epoch_i = prop_params.epoch
+    dt = (epoch_f - epoch_i)/ (n+1)
+    output = []
+    for i in range(0, n + 2):
+        desired_epoch = epoch_i + dt * i
+        obs_pos_eci = ecef_to_eci(obs_pos_ecef, desired_epoch)
+        obj_pos_eci = state_propagate(x, desired_epoch, prop_params)
+        rr = obj_pos_eci - obs_pos_eci
+        angles = local_angles(rr, obs_pos_lla)
+        output.append([angles[0], angles[1], desired_epoch])
+    return output
