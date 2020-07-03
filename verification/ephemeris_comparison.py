ba45167f341df8c01d3508d36663e1b534c7072a
aogle <aogle@localhost.localdomain>
2020-07-02 22:48:42 -0400

    Added file ot verify ephermis results with my functions

---
+++ verification/ephemeris_comparison.py
@@ -0,0 +1,32 @@
+import math
+import numpy as np
+from astropy.time import Time
+import astropy.units as u
+from verification.util import get_satellite_position_over_time, build_observations, build_epochs
+from src.interface.tle import tle_to_state
+from src.enums import Frames
+from src.interface.local_angles import get_local_angles_for_state_propagation
+
+
+tle_string = """
+2020-040A
+1 45807U 20040A   20175.61197145 -.00009734  00000-0 -10000-3 0  9998
+2 45087  28.4194 294.5058 7294772 179.9989 70.3936 2.28021460    11
+"""
+# 1 45807U 20040A   20175.61197145 -.00000151 00000-0 -16612-3 0  9998
+
+x, params = tle_to_state(tle_string)
+epoch = Time("2020-07-03T05:00:00.000", format="isot", scale="utc")
+params.epoch = epoch
+obs_pos = [29.218103 * u.deg, -81.031723 * u.deg, 0 * u.km]
+
+epochs = build_epochs(epoch, 15 * u.min, 4)
+observations = build_observations(x, params, obs_pos, Frames.LLA, epochs)
+
+for obs in observations:
+    print(obs.obs_values)
+
+n = len(epochs)
+locals = get_local_angles_for_state_propagation(x, params, epochs[n-1], n-2, obs_pos, Frames.LLA)
+for local in locals:
+    print(local)
--- verification/testing_eci_ecef_with_groundtracks.py
+++ verification/testing_eci_ecef_with_groundtracks.py
@@ -4,10 +4,13 @@ import numpy as np
 from astropy.time import Time
 import astropy.units as u
 from verification.util import get_satellite_position_over_time
-from src.frames import eci_to_ecef, ecef_to_lla
+from src.frames import eci_to_ecef, ecef_to_lla, ecef_to_eci
 import matplotlib.pyplot as plt
 
-epoch = Time(2454283.0, format="jd", scale="tdb")
+# epoch = Time("2000-01-01T00:00:00.000", format="isot", scale="utc")
+# epoch.format = "jd"
+day_val = 1/365.25
+epoch = Time(1984, format='decimalyear', scale="utc")
 period = 86164.1                     # Seconds in a sidereal day
 dt = period/100
 tf = period
@@ -15,8 +18,14 @@ t = np.arange(0, tf, dt)
 n = t.shape[0]
 a = math.pow(math.pow(period/2/math.pi, 2) * mu.value, 1/3)
 speed = math.sqrt(mu.value/a)
-
+r_0 = np.array([a, 0, 0])
 x = np.array([a, 0, 0, 0, speed, 0])
+# norm = np.linalg.norm(x)
+# r_0eci = ecef_to_eci(r_0, epoch)
+# v_0eci = np.cross(r_0eci, np.array([0, 0, speed])) / np.linalg.norm(r_0eci)
+# x = np.concatenate([r_0eci, v_0eci])
+# nomr2 = np.linalg.norm(x)
+# x = np.array([r_0eci[0], r_0eci[1], r_0eci[2], v_0_eci[0], v_0_eci[1], v_0_eci[2]])
 r_eci = get_satellite_position_over_time(x, epoch, tf, dt)
 
 r_ecef = np.zeros((n, 3))
@@ -34,9 +43,11 @@ ax.set_ylabel('Latitude [deg]')
 ax.set_xlabel('Longitude [deg]')
 plt.show()
 
-fig = plt.figure(2)
-ax = fig.gca()
-plt.plot(t, r_lla[:, 2])
-ax.set_xlabel('time [s]')
-ax.set_ylabel('Altitude [km]')
-plt.show()
+# fig = plt.figure(2)
+# ax = fig.gca()
+# plt.plot(t, r_lla[:, 2])
+# ax.set_xlabel('time [s]')
+# ax.set_ylabel('Altitude [km]')
+# plt.show()
+
+print(r_lla[:, 1])
